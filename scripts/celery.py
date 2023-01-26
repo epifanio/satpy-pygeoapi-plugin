1c3d62a008335123699a75f196c6cf479da4159c
epifanio <epiesasha@me.com>
2023-01-26 09:08:54 +0100

    read redis host:port from os.env

--- scripts/celery.py
+++ scripts/celery.py
@@ -1,14 +1,23 @@
 from celery import Celery
+import os
 
-app = Celery('scripts',
-            broker='redis://',
-            backend='redis://',
-            result_backend='redis://',
-            result_extended=True,
-            include=['satpy_pygeoapi_plugin.process_netcdf'])
 
-app.conf.update(result_expires=3600,)
+redis_host = os.environ.get("REDIS_HOST", "redis")
+redis_port = os.environ.get("REDIS_PORT", 6379)
 
-if __name__ == '__main__':
+app = Celery(
+    "scripts",
+    broker=f"redis://{redis_host}:{redis_port}",
+    backend=f"redis://{redis_host}:{redis_port}",
+    result_backend=f"redis://{redis_host}:{redis_port}",
+    result_extended=True,
+    include=["satpy_pygeoapi_plugin.process_netcdf"],
+)
+
+app.conf.update(
+    result_expires=3600,
+)
+
+if __name__ == "__main__":
     # Optional configuration, see the application user guide.
     app.start()
